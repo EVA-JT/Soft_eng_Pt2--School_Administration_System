07d4879e6d39b900eb60608f84fd1e2ced795cc7
Tenorio <jvtv@ic.ufal.br>
2025-05-13 03:14:25 -0300

    Update dados.py

--- dados/dados.py
+++ dados/dados.py
@@ -1,7 +1,7 @@
-gradebook = [
+gradebook = [ #banco de boletins
     {'matricula': '0001', 'math': {'ab1': 8, 'ab2': 0}, 'science': {'ab1': 2, 'ab2': 0}},
 ]
-users = {
+users = { #banco de usuarios
     1:("tutor", "badu", "admin_badu", "projeto"),
     2:("student", "joao", "joaoPassword", 0, "a1", 3),
     3:("student","arthur", "arthurPassword", 10, "a1", 3)
